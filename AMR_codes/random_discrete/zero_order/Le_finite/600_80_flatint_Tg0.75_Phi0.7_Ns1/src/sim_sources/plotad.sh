2c10ed05c13c82c1edd86884453f036a71fce874
Hossein Azizi <hossein.azizi@mail.mcgill.ca>
2017-12-07 15:50:18 -0500

    Modified files

--- AMR_codes/random_discrete/zero_order/Le_finite/600_80_flatint_Tg0.75_Phi0.7_Ns1/src/sim_sources/plotad.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#PBS -l nodes=1:ppn=16 -l pmem=3700m -l walltime=00:24:00:00
-#PBS -A fyw-423-ad
-#PBS -N plotad
-#PBS -o plotad.log
-#PBS -e plotad_err.log
-cd $PBS_O_WORKDIR
-python plotad_comov.py
-
--- Archive/May28_2015/sim_address.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-clear
-
- echo -n "Going to following address : " "gs/scratch/hophy/Simulation_results"
- echo -n
- cd /gs/sctarch/
